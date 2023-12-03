9ea79ed5e744d34921d60b07fcd42397fa5b86d7
panto72 <prantoa80@gmail.com>
2023-12-04 01:52:57 +0600

    04/12/2023

---
+++ Employe.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employe_salary
+{
+    internal class Employe:Person
+    {
+        public string Designation {  get; set; }
+        public string Branch { get; set; }
+        public int Basic { get; set; }    
+        public int Insentive { get; set; }    
+        public int TA {  get; set; }
+        public int PF {  get; set; }
+        public int Total_salary {  get; set; }
+
+        //public Employe() { }
+        public double Salary()
+        {
+            
+            Insentive =(int) (Basic * 0.5);
+            TA = (int)(Basic * 0.2);
+            PF = (int) (Basic * 0.4);
+            Total_salary = Basic + Insentive + TA-PF;
+            return Total_salary;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using Employe_salary;
+using System;
+public class Program
+{
+    public static void Main()
+    {
+        List<Employe> employes = new List<Employe>
+        {
+            new Employe { Name = "Mafuj", Id = 14172, Phone = "0175233251", Email = "pranto@gmail.com", Address = "Dhanmondi", City = "Dhaka", Designation = "CEO", Branch = "Dhanmondi", Basic = 8000 },
+            new Employe
+            {
+                Name = "John Doe",
+                Id = 1,
+                Phone = "123-456-7890",
+                Email = "john.doe@example.com",
+                Address = "123 Main St",
+                City = "Anytown",
+                Designation = "Developer",
+                Branch = "Headquarters",
+                Basic = 50000 // Set the basic salary for John Doe
+            }
+        };
+
+        foreach (var Employess in employes)
+        {
+            Console.WriteLine("Enter ID:");
+            int n = int.Parse(Console.ReadLine());
+            if (Employess.Id ==n)
+            {
+                Console.WriteLine(Employess.Name);
+                Console.WriteLine(Employess.Email);
+                break;
+            }
+        }
+
+        foreach (var employe in employes)
+        {
+            Double totalSalary = employe.Salary();
+            Console.WriteLine($"{employe.Name}'s salary is:{totalSalary}");
+        }
+
+    }
+}
